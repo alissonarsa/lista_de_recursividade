4aabb0831e07c22a25fa3e3b197df73430f59e96
alissonarsa <alissonarsa@gmail.com>
2025-03-11 19:48:26 -0300

    criado valor do dolar mensal e projeto de função

---
+++ 4.py
@@ -0,0 +1,27 @@
+dolar2024 = { # fonte: https://idealsoftwares.com.br/indices/dolar2024.html
+    "1": 4.89,
+    "2": 4.93,
+    "3": 4.95,
+    "4": 5.05,
+    "5": 5.11,
+    "6": 5.23,
+    "7": 5.58,
+    "8": 5.66,
+    "9": 5.62,
+    "10": 5.45,
+    "11": 5.80,
+    "12": 6.06
+}
+
+def deposita_na_poupança_converte_em_dolar_mensal(valor):
+    saldo = 0
+
+    # acessar valor da chave para do mÊs de acordo com a recursividade
+    conversao = valor / taxa
+    saldo += valor
+
+    # receber rendimento na conta
+
+    # depositar na conta
+
+    return saldo
